bee7d847a59dc023e7db7ea942018e6f7e55ed09
julianKim11 <juliankim0911@gmail.com>
2023-11-13 17:32:09 -0300

    cambios menores

---
+++ Assets/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager1 : MonoBehaviour
+{
+    public GameObject _menuGameOver;
+
+    [SerializeField] private GameObject _playerOneVictory;
+    [SerializeField] private GameObject _playerTwoVictory;
+    [SerializeField] private GameObject _tie;
+
+}
