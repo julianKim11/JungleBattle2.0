80cc08e0adc5cfa20f41c64140408c98a82da562
julianKim11 <juliankim0911@gmail.com>
2023-11-13 18:47:58 -0300

    menu, timer

---
+++ Assets/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] public GameObject _menuGameOver;
+    [SerializeField] private GameObject _tie;
+    [SerializeField] private GameObject _playerOneVictory;
+    [SerializeField] private GameObject _playerTwoVictory;
+}
---
+++ Assets/Assets/Scripts/UI/TimeController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TimeController : MonoBehaviour
+{
+    [SerializeField] int min, seg;
+    [SerializeField] private TMP_Text _time;
+    private float _left;
+    private bool _onGoing;
+    private void Awake()
+    {
+        _left = (min * 60) + seg;
+        _onGoing = true;
+    }
+    private void Update()
+    {
+        if (_onGoing)
+        {
+            _left -= Time.deltaTime;
+            if (_left < 1)
+            {
+                Debug.Log("Empate");
+                _onGoing = false;
+            }
+            int tempMin = Mathf.FloorToInt(_left / 60);
+            int tempSeg = Mathf.FloorToInt(_left % 60);
+            _time.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
+        }
+    }
+}
